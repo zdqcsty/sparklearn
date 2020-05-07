d73acab3efde21712478cd47a1cd3cb9e159b016
zengguanghao <zengguanghao@bonc.com.cn>
2020-05-07 20:18:37 +0800

    spark

---
+++ src/main/java/com/example/sparklearn/Test.scala
@@ -0,0 +1,23 @@
+package com.example.sparklearn
+
+import org.apache.spark.sql.SparkSession
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+
+    val session = SparkSession
+      .builder().enableHiveSupport().getOrCreate()
+
+    val frame = session.read.csv("hdfs://10.129.18.17:8020/user/zgh/test1.csv")
+
+
+
+    frame.show()
+    frame.write.csv("/user/zgh/test")
+
+    session.close()
+
+
+  }
+}
