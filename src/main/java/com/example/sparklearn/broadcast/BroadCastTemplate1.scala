0e4f86e3ee942e72089e799cc8ec8026b9b3c375
zengguanghao <zeng92922@163.com>
2020-07-06 15:16:49 +0800

    清理无效代码

--- src/main/java/com/example/sparklearn/broadcast/BroadCastTemplate1.scala
+++ src/main/java/com/example/sparklearn/broadcast/BroadCastTemplate1.scala
@@ -13,6 +13,7 @@ object BroadCastTemplate1 {
 
     val gongcan = session.sql("select * from ceshi.gongcan")
 
+    //这种方式相当于先广播然后注册成一张临时表
     val value = broadcast(gongcan).createOrReplaceTempView("gongcan")
 
 //    session.sql("create table ceshi.iklouytg as select /*+ BROADCAST(a) */ b.* from zgh.vbapf5f29d8a8a724888a9069d7c96d6cf22 a join demo b on a.column_0=b.id")
