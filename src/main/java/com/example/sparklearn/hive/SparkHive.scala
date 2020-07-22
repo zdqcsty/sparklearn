d0c90848d775cfd01101b8e1b51d786f3b466ded
zengguanghao <zeng92922@163.com>
2020-07-22 11:18:32 +0800

    spark在本地以hadoop用户访问hive

---
+++ src/main/java/com/example/sparklearn/hive/SparkHive.scala
@@ -0,0 +1,33 @@
+package com.example.sparklearn.hive
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.SparkSession
+
+object SparkHive {
+
+  def main(args: Array[String]): Unit = {
+
+    //设置hadoop用户   必须在sparksession创建前设置 不然不生效
+    System.setProperty("HADOOP_USER_NAME", "hadoop")
+
+    val session = SparkSession
+      .builder().enableHiveSupport().master("local[*]").getOrCreate()
+
+    //要显示指定core-site.xml hdfs-site.xml  hive-site.xml  或者将他放到resource根目录下
+    val conf=new Configuration
+    conf.addResource("hebing/core-site.xml")
+    conf.addResource("hebing/hdfs-site.xml")
+    conf.addResource("hebing/hive-site.xml")
+    session.sparkContext.hadoopConfiguration.addResource(conf);
+
+    session.sparkContext.hadoopConfiguration.set("dfs.client.use.datanode.hostname", "true")
+
+    session.sql("insert into ceshi.streaming select * from ceshi.test limit 10")
+
+    session.close()
+
+
+
+  }
+
+}
