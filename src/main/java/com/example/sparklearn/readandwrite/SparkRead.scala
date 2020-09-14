1e6bb2ddf4dc7a6fdc16d1e17ed21f178c4a8e90
zengguanghao <zeng92922@163.com>
2020-09-14 10:59:29 +0800

    spark读取tsv文件（修复）

--- src/main/java/com/example/sparklearn/readandwrite/SparkRead.scala
+++ src/main/java/com/example/sparklearn/readandwrite/SparkRead.scala
@@ -1,6 +1,6 @@
 package com.example.sparklearn.readandwrite
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SparkRead {
 
@@ -8,25 +8,22 @@ object SparkRead {
 
     val spark = SparkSession
       .builder().master("local").enableHiveSupport().getOrCreate()
-/*
+
     //读取csv  (本地测试需要使用全路径)
     val csvframe = spark.read.csv("E:\\study_workSpace\\sparklearn\\src\\main\\resources\\file\\test1.csv")
-    csvframe.show()*/
+    csvframe.show()
 
     //读取tsv文件(制表符分隔)    (参考https://xbuba.com/questions/43508054)
-    val tsvFrame = spark.read.option("header", true).format("csv").option("delimiter", "\\t")
-      .load("E:\\study_workSpace\\sparklearn\\src\\main\\resources\\file\\test2.tsv")
-
-
+    val tsvFrame = readTsv(spark,"E:\\study_workSpace\\sparklearn\\src\\main\\resources\\file\\test2.tsv")
     tsvFrame.show()
-
-//    tsvFrame.write.option("header","true").csv("file:///E:\\data.csv")
-
-//    frame.write.csv("F:\\aaa")
-
 
     spark.close()
   }
 
+  //读取tsv文件(制表符分隔)(参考https://xbuba.com/questions/43508054)
+  def readTsv(spark: SparkSession, path: String): DataFrame = {
+    spark.read.option("header", true).format("csv").option("delimiter", "\\t")
+      .load(path)
+  }
 
 }
